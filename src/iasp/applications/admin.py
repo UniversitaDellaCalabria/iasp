14f05e8c3bb44b6950aba69688f579af72e9a30b
Francesco Filicetti <easyweb.wa@gmail.com>
2025-07-14 16:29:38 +0200

    feat: application model admin

--- src/iasp/applications/admin.py
+++ src/iasp/applications/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import *
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'call',
+        'submission_date',
+        'protocol_number',
+        'protocol_date',
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
